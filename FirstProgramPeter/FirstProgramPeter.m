efcc9519cfc6edda29c847ed6c31a39a30b9bae0
nphil <nphil@10.252.213.181>
2018-07-15 19:13:01 -0400

    development branch project structure

---
+++ FirstProgramPeter/FirstProgramPeter.m
@@ -0,0 +1,16 @@
+(* Wolfram Language Package *)
+
+(* Created by the Wolfram Workbench Jul 14, 2018 *)
+
+BeginPackage["FirstProgramPeter`"]
+(* Exported symbols added here with SymbolName::usage *) 
+
+Begin["`Private`"]
+(* Implementation of the package *)
+
+
+
+End[]
+
+EndPackage[]
+
